c1fe3200d0f6dcd20f51a10766e8c36b79c349fe
Wensheng ZHANG <wensheng.zhang@instant.co>
2019-01-11 14:02:38 -0500

    rename package bump up version

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "meteor:camera",
+  name: "instant:meteor-camera",
   summary: "Photos with one function call on desktop and mobile.",
   version: "1.4.2",
-  git: "https://github.com/meteor/mobile-packages"
+  git: "https://github.com/zhang2118/meteor-camera.git"
 });
 
 Cordova.depends({
